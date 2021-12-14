55eb776bad2563e98196fb48aefbf05ab826e31b
Vejni <vmarcell000@gmail.com>
2021-12-14 13:47:05 +0100

    roots working

--- feature_term_gens.py
+++ feature_term_gens.py
@@ -8,6 +8,22 @@ def init_FeatStruct(root, features=None, **morefeatures):
     fs.__setattr__("root", root)
     return  fs
 
+def fs_copy(fs):
+    def _fs_copy(temp, fs):
+        for (_, temp_val), (_, val) in zip(temp.items(), fs.items()):
+            if isinstance(val, FeatStruct):
+                temp_val.__setattr__("root", val.root)
+                _fs_copy(temp_val, val)
+
+
+    temp = fs.copy()
+    temp.__setattr__("root", fs.root)
+
+    # Set attributes again
+    _fs_copy(temp, fs)
+
+    return temp
+
 
 def find_struct_matching(fs, test, path = []):
     """ 
@@ -27,7 +43,7 @@ def find_struct_matching(fs, test, path = []):
     return None
 
 
-def find_matching(fs, all_paths, test, path = []):
+def find_matching(fs, test, path = []):
     """ 
     Finds the path and value of the first node matching our criterion
     in depth first search order. Works recursively. 
@@ -36,30 +52,31 @@ def find_matching(fs, all_paths, test, path = []):
     """
     for feature, value in fs.items():
         if isinstance(value, FeatStruct):
-            p = find_matching(value, all_paths, test, path + [feature])
-            if p is not None and p not in all_paths:
+            p = find_matching(value, test, path + [feature])
+            if p is not None:
                 return p
             elif test(value.root):
-                p = path + [feature]
-                if p not in all_paths:
-                    return p
+                return path + [feature]
         else:
             if test(value):
-                p = tuple(path + [feature, value])
-                if p not in all_paths:
-                    return p
+                return tuple(path + [feature, value])
     return None
 
 def find_all(fs, test):
     """ Finds all paths to nodes which have supersorts by calling find_matching iteratively using the correct test """
+    fs = fs_copy(fs)
     all_paths = []
-    while (path := find_matching(fs, all_paths, test = test)) is not None:
+    while (path := find_matching(fs, test = test)) is not None:
         all_paths.append(path)
+        if isinstance(path, tuple):
+            fs[path[:-1]] = None
+        else:
+            fs[tuple(path)].root = None
     return all_paths
 
 def find_all_structs(fs):
     """ Finds all paths to non-leaf nodes by calling find_struct_matching iteratively using the correct test """
-    fs = fs.copy()
+    fs = fs_copy(fs)
     all_branch_paths = []
     while (path := find_struct_matching(fs, test = lambda x: True)) is not None:
         all_branch_paths.append(path)
@@ -67,7 +84,7 @@ def find_all_structs(fs):
     return  all_branch_paths
 
 def gen_step(fs, sorts):
-    fs = fs.copy()
+    fs = fs_copy(fs)
     if (path := find_matching(fs, test = lambda x: x in sorts.keys())) is not None:
         fs[path[:-1]] = sorts[path[-1]]
     return fs
@@ -76,8 +93,11 @@ def get_all_sort_generalizations(fs, sorts):
     paths = find_all(fs, test = lambda x: x in sorts.keys())
     result = []
     for p in paths:
-        temp = fs.copy()
-        temp[p[:-1]] = sorts[p[-1]]
+        temp = fs_copy(fs)
+        if isinstance(p, tuple):
+            temp[p[:-1]] = sorts[p[-1]]
+        else:
+            temp[tuple(p)].root = sorts[temp[tuple(p)].root]
         result.append(temp)
     return result
 
@@ -85,8 +105,11 @@ def get_all_variable_eliminations(fs, sorts):
     paths = find_all(fs, test = lambda x: x in sorts.values())
     result = []
     for p in paths:
-        temp = fs.copy()
-        del temp[p[:-1]]
+        temp = fs_copy(fs)
+        if isinstance(p, tuple):
+            del temp[p[:-1]]
+        else:
+            del temp[tuple(p)]
         result.append(temp)
     return result
     
@@ -107,5 +130,5 @@ if __name__ == "__main__":
     }
 
 
-    print(icon1)
-    print(get_all_sort_generalizations(icon1, sorts))
+    fs = get_all_sort_generalizations(icon1, sorts)[1]
+    print(get_all_variable_eliminations(fs, sorts))
